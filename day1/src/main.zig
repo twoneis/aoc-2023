1848a039f518513221013171acf02f4699a765d3
twoneis <sanjay29@sapura.de>
2023-12-01 21:09:56 +0100

    even more clean

--- day1/src/main.zig
+++ day1/src/main.zig
@@ -8,15 +8,53 @@ pub fn main() !void {
 
     const file_name = args.next().?;
 
+    const solution_one = try part_one(file_name);
+    const solution_two = try part_two(file_name);
+
+    try stdout.print("Solution part one: {d}\nSolution part two: {d}\n", .{ solution_one, solution_two });
+}
+
+fn part_one(file_name: []const u8) !u32 {
     var file = try std.fs.cwd().openFile(file_name, .{});
     defer file.close();
 
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
 
-    var sum: i32 = 0;
+    var sum: u32 = 0;
 
+    var buf: [1024]u8 = undefined;
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        var first_num: u8 = undefined;
+        var last_num: u8 = undefined;
+        var first = true;
+        for (line, 0..) |_, i| {
+            if (std.ascii.isDigit(line[i])) {
+                if (first) {
+                    first_num = line[i];
+                    first = false;
+                }
+                last_num = line[i];
+            }
+        }
+        const num_str = [_]u8{ first_num, last_num };
+        const num = try std.fmt.parseInt(u32, &num_str, 10);
+        sum += num;
+    }
+
+    return sum;
+}
+
+fn part_two(file_name: []const u8) !u32 {
     const alpha_digits = [_][]const u8{ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+    var file = try std.fs.cwd().openFile(file_name, .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+
+    var sum: u32 = 0;
 
     var buf: [1024]u8 = undefined;
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
@@ -39,8 +77,9 @@ pub fn main() !void {
             }
         }
         const num_str = [_]u8{ first_num, last_num };
-        const num = try std.fmt.parseInt(i32, &num_str, 10);
+        const num = try std.fmt.parseInt(u32, &num_str, 10);
         sum += num;
     }
-    try stdout.print("{d}\n", .{sum});
+
+    return sum;
 }
